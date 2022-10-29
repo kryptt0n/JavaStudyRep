76977836b679661b793564619695c431833ef95c
kryptt0n <vitaliksuhinin99@gmail.com>
2022-10-29 23:37:58 +0300

    added class for serialisation trying new features

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,18 @@
+import com.vitaly.interfaces.FunctionalIntTest;
 import java_folder.Client;
+import java_folder.JetBrainsEntryExam;
 import java_folder.Person;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.regex.Pattern;
+
+import classes_folder.LeetCodeTasks;
+import com.vitaly.interfaces.FunctionalIntTest;
 
 /**
  * Documentation program helloworld
@@ -7,32 +20,83 @@ import java_folder.Person;
  * @version 1.0
  */
 
-
 public class HelloWorld {
     final static Integer inta = 1;
-    public static void main(String[] args)  {
-        Hehe lolo = new Lolo();
 
+    public static void doShit(int a, int b, FunctionalIntTest ft) {
+        if (ft.wtf(a, b) > 0) {
+            System.out.println(a + ">" + b);
+        } else if (ft.wtf(a, b) < 0){
+            System.out.println(a + "<" + b);
+        } else {
+            System.out.println(a + "=" + b);
+        }
+    }
+    public static void main(String[] args) {
+//        System.out.println(LeetCodeTasks.canConstruct("bg","efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj"));
+        String test = "Acdw Z";
+        System.out.println(test.replaceAll("[A-Z]", ""));
+        System.out.println(Pattern.compile("[A-Z]").matcher(test));
+    }
+
+    static int andrewAndAcid() {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        ArrayList<Integer> acid = new ArrayList<>();
+        int answer = 0;
+        for (int i = 0; i < size; i++) {
+            acid.add(scanner.nextInt());
+        }
+
+        int max = acid.get(0);
+        for (int i = 0; i < acid.size(); i++) {
+            max = Math.max(acid.get(i), max);
+            if (acid.get(i) < max) {
+                answer = -1;
+                break;
+            }
+        }
+        if (answer == 0) {
+            return acid.stream().max((a, b) -> a - b).get() - acid.stream().min((a, b) -> a - b).get();
+        } else
+            return answer;
 
     }
 
-    public static void changePerson(Person p){
-        p.setName("Vitos");
+    static boolean isSorted (ArrayList<Integer> arrayList) {
+        for (int i = 1; i < arrayList.size(); i++) {
+            if (arrayList.get(i) < arrayList.get(i - 1))
+                return false;
+        }
+        return true;
     }
+
+
+
 }
 
-class Hehe {
-    public void sayhi(){
-        System.out.println("hi");
+
+
+class Runner implements Runnable {
+
+    private static int counter;
+
+    public int id;
+
+    public Runner() {
+        counter++;
+        id = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("" + id + " goes " + i + " time");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+
+            }
+        }
     }
 }
-
-class Lolo extends Hehe {
-    public void sayhi(int lol){
-        System.out.println("lolo");
-    }
-    public void test(){
-        System.out.println("test");
-    }
-}
-
