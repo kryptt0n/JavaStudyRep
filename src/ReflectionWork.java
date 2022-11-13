bddeee353183555affe56c1856bb20f331b1dbe1
kryptt0n <vitaliksuhinin99@gmail.com>
2022-11-13 20:24:48 +0300

    separated code into different methods to improve comprehension

--- src/ReflectionWork.java
+++ src/ReflectionWork.java
@@ -1,11 +1,23 @@
 import com.vitaly.classes.Person;
+import com.vitaly.classes.PrivateClass;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class ReflectionWork {
     public static void main(String[] args) {
+        try {
+            workWithPrivateAttributes();
+        } catch (Exception e) {
+            System.out.println("Ошибка");
+        }
+    }
+
+    private static void workWithClasses() {
         try {
             //Получение класса зная класс до компиляции
             Class cl = HelloWorld.class;
@@ -35,5 +47,64 @@ public class ReflectionWork {
         } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
             System.out.println("uuops");
         }
+    }
+
+    private static void workWithFields() {
+        try {
+            Class classpersona = Person.class;
+            //Получить все public поля
+            Field[] fields = classpersona.getFields();
+            //Получить public поле по имени
+            Field specificOne = classpersona.getField("retirementAge");
+            System.out.println(Arrays.toString(fields));
+            //Получить тип поля
+            Object fieldType = specificOne.getType();
+            System.out.println(fieldType);
+            //Установка значения поля для конкретного объекта
+            Constructor specificConstructor = classpersona.getConstructor(int.class, String.class);
+            Person newPerson = (Person) specificConstructor.newInstance(15, "Vitos");
+            specificOne.set(newPerson, 32);
+            System.out.println(newPerson.retirementAge);
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    private static void workWithMethods() {
+        try {
+            Class persona = Person.class;
+            //Получение всех public методов
+            Method[] methods = persona.getMethods();
+            System.out.println(Arrays.toString(methods));
+            Method specificMethod = persona.getDeclaredMethod("sayHello");
+            Class[] paramTypes = specificMethod.getParameterTypes();
+            Class returnType = specificMethod.getReturnType();
+
+            System.out.println(paramTypes + " " + returnType);
+        } catch (Exception e) {
+
+        }
+    }
+
+    private static void workWithPrivateAttributes() throws Exception {
+        Class privateClass = PrivateClass.class;
+        //получить ВСЕ методы (даже приватные)
+        Method[] privateMethods = privateClass.getDeclaredMethods();
+        //получить ВСЕ поля
+        Field[] privateFields = privateClass.getDeclaredFields();
+        System.out.println(privateFields + "\n" + privateMethods);
+        Field specificField = privateClass.getDeclaredField("number");
+        Constructor constructor = privateClass.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        PrivateClass instance = (PrivateClass) constructor.newInstance();
+        //Отключаем обязательно проверку, иначе компилятор не даст работать с приват полем
+        specificField.setAccessible(true);
+        //Получаем значение поля
+        System.out.println(specificField.get(instance));
+        //Меняем значение поля
+        specificField.set(instance, 50);
+        System.out.println(specificField.get(instance));
+
     }
 }
---
+++ src/com/vitaly/classes/PrivateClass.java
@@ -0,0 +1,13 @@
+package com.vitaly.classes;
+
+public class PrivateClass {
+
+    private int number = 24;
+
+    private PrivateClass() {}
+
+    private void method() {
+        System.out.println("Im private");
+    }
+
+}
